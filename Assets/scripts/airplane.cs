217c6a9ca74f7d541d847a721f5fa3b86a28bcdf
RK <irmareka@gmail.com>
2017-08-13 18:46:13 -0700

    adding metas

--- Assets/scripts/airplane.cs
+++ Assets/scripts/airplane.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class airplane : MonoBehaviour {
 
   public GameObject Explosion;
 
+  public Text countText;
+  public Text winText;
+  private int count;
+
+  void Start() {
+    count = 0;
+    SetCountText();
+    winText.text = "";
+  }
   void OnTriggerEnter()
   {
     Vector3 temp = transform.position;
     temp.y += 10f;
     Instantiate(Explosion, temp, Quaternion.identity);
     Destroy(this.gameObject);
+    count += 1;
+    SetCountText();
+  }
+
+  void SetCountText() {
+    countText.text = "Score: " + count.ToString();
+    if (count >= 15)  {
+      winText.text = "You Win!";
+    }
   }
 
 }
